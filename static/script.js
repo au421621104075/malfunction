5fd97bd5bdf6459c8e2468ff31bc98b9a9b3e92a
au421621104075 <rlavanyaramesh1808@gmail.com>
2025-11-29 11:45:56 +0530

    fourth commit

--- static/script.js
+++ static/script.js
@@ -1,6 +1,13 @@
 console.log("Traffic Signal Dashboard Loaded");
 
 // Auto-refresh every 30 seconds
-setTimeout(() => {
-    window.location.reload();   // tells the browser to refresh the current page.
-}, 30000); //30000 is the delay in milliseconds (30,000 ms = 30 seconds).
+let timeLeft = 10;
+
+setInterval(() => {
+    timeLeft--;
+    document.getElementById("timer").innerText = timeLeft;
+
+    if (timeLeft <= 0) {
+        window.location.reload();
+    }
+}, 1000);
